ecb2a12471e0cd72ee6cd94a8ee8dabe4a2ce60c
charanjeetkaur04 <charan.agt@gmail.com>
2025-08-04 17:41:42 +0530

    working on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,349 @@
-<h3>Employee Leave Management System </h3>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Employee Leave Management System</title>
+
+    <!--owl slider stylesheet -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <!-- fonts awesome style -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <!-- fonts style -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,500,700&display=swap" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="contact_nav_container">
+                <div class="container">
+                    <div class="contact_nav">
+                        <a href="">
+                            <i class="fa fa-map-marker" aria-hidden="true"></i>
+                            <span>
+                                Address : 69 W 9th St, New York, NY 10011, USA
+                            </span>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                            <span>
+                                Email : emls@gmail.com
+                            </span>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-phone" aria-hidden="true"></i>
+                            <span>
+                                Phone Call : +01 123754678990
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-lg custom_nav-container">
+                    <!-- <a class="navbar-brand" href="index.html"> -->
+                    <a class="navbar-brand" href="index.html">
+                        <span>
+                            Employee Leave Management System
+                        </span>
+                    </a>
+                    <div class="user_option">
+                        <a href="">
+                            <span>
+                                Login
+                            </span>
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                        </a>
+                        <!-- <form class="form-inline">
+                            <button class="btn  nav_search-btn" type="submit">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </button>
+                        </form> -->
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <!-- end header section -->
+        <!-- slider section -->
+        <section class="slider_section position-relative">
+        </section>
+        <!-- end slider section -->
+    </div>
+
+    <!-- service section -->
+
+    <!-- <section class="service_section ">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="box">
+                        <div class="img-box">
+                            <img src="images/service1.png" alt="" />
+                        </div>
+                        <div class="detail-box">
+                            <h4>
+                                Baby Milk
+                            </h4>
+                            <p>
+                                available, but the majority have suffered alteration in some form, by injected
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="box ">
+                        <div class="img-box">
+                            <img src="images/service2.png" alt="" />
+                        </div>
+                        <div class="detail-box">
+                            <h4>
+                                Baby Clothes
+                            </h4>
+                            <p>
+                                available, but the majority have suffered alteration in some form, by injected
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="box">
+                        <div class="img-box">
+                            <img src="images/service3.png" alt="" />
+                        </div>
+                        <div class="detail-box">
+                            <h4>
+                                Baby
+                            </h4>
+                            <p>
+                                available, but the majority have suffered alteration in some form, by injected
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="box">
+                        <div class="img-box">
+                            <img src="images/service4.png" alt="" />
+                        </div>
+                        <div class="detail-box">
+                            <h4>
+                                Baby Walk
+                            </h4>
+                            <p>
+                                available, but the majority have suffered alteration in some form, by injected
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+    <section class="service_section">
+        <div class="container">
+            <div class="row">
+                <!-- Leave Application -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="box">
+                        <div class="img-box">
+                            <img src="images/clipboard.png" alt="Leave Application" />
+                        </div>
+                        <div class="detail-box">
+                            <h4>Apply for Leave</h4>
+                            <p>Employees can easily apply for various leave types with just a few clicks.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Leave Approval -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="box">
+                        <div class="img-box">
+                            <img src="images/approval.png" alt="Leave Approval" />
+                        </div>
+                        <div class="detail-box">
+                            <h4>Admin Approval</h4>
+                            <p>Leave requests are reviewed and approved by managers or HR in real time.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Leave Balance Tracking -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="box">
+                        <div class="img-box">
+                            <img src="images/leave.png" alt="Leave Balance" />
+                        </div>
+                        <div class="detail-box">
+                            <h4>Leave Balance</h4>
+                            <p>Track remaining and used leaves for every employee, across leave types.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Reports & History -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="box">
+                        <div class="img-box">
+                            <img src="images/reporting.png" alt="Reports" />
+                        </div>
+                        <div class="detail-box">
+                            <h4>Reports & History</h4>
+                            <p>Detailed reports of leave history for employees, departments, and admins.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- end service section -->
+
+    <!-- about section -->
+
+    <section class="about_section layout_padding">
+        <div class="container-fluid">
+            <div class="box">
+                <div class="img-box">
+                    <img src="images/index2.jpg" alt="" />
+                </div>
+                <div class="detail-box">
+                    <h2>
+                        About Us
+                    </h2>
+                    <p>
+                        Welcome to the Employee Leave Management System, a streamlined solution designed to simplify and automate leave tracking and approval within any organization.
+
+                        Our platform helps HR departments and managers efficiently manage employee absences, and more — all in one centralized system.
+
+                        We built this system to reduce manual paperwork, prevent scheduling conflicts, and empower employees with self-service leave applications.
+
+                        At our core, we aim to make employee leave management simple, accurate, and stress-free platform. </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end about section -->
+
+    <!-- contact section -->
+    <!--
+    <section class="contact_section layout_padding-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <div class="contact-form">
+                        <div class="heading_container">
+                            <h2>
+                                Get In Touch
+                            </h2>
+                        </div>
+                        <form action="">
+                            <input type="text" placeholder="Full name " />
+                            <div class="top_input">
+                                <input type="email" placeholder="Email" />
+                                <input type="text" placeholder="Phone Number" />
+                            </div>
+                            <input type="text" placeholder="Message" class="message_input" />
+                            <div class="btn-box">
+                                <button>
+                                    Send
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+
+    <!-- end contact section -->
+
+    <!-- info section -->
+
+    <section class="info_section layout_padding2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="info_contact">
+                        <h5>
+                            Address
+                        </h5>
+                        <div class="contact_link_box">
+                            <a href="">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                <span>
+                                    69 W 9th St, New York, NY 10011, USA
+                                </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <span>
+                                    Call +01 1234567890
+                                </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                <span>
+                                    elms@gmail.com
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="info_social">
+                        <a href="">
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+                        </a>
+                        <a href="">
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end info section -->
+
+    <!-- footer section -->
+    <footer class="footer_section container-fluid">
+        <p>
+            &copy; <span id="displayYear"></span> All Rights Reserved.
+        </p>
+    </footer>
+    <!-- footer section -->
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
